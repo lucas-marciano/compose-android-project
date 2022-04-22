f48a77feba7dab786086332aa4ad3b9f7797a706
lucas-marciano <lucas.almeida.marciano@gmail.com>
2022-04-22 11:30:43 -0300

    feat: make correction in koin

--- app/src/main/java/com/lucasmarciano/composeproject/BaseAppApplication.kt
+++ app/src/main/java/com/lucasmarciano/composeproject/BaseAppApplication.kt
@@ -5,13 +5,14 @@ import com.lucasmarciano.composeproject.di.modules.featureModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class BaseAppApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
         startKoin {
-            androidLogger()
+            androidLogger(if (BuildConfig.DEBUG) Level.ERROR else Level.NONE)
             androidContext(this@BaseAppApplication)
             modules(
                 listOf(
