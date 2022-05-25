7f28ba5ac3c4ab94dd3579b69902d307ca680dfd
lucas-marciano <lucas.almeida.marciano@gmail.com>
2022-05-25 14:57:23 -0300

    chore: removed unused files

--- app/src/main/java/com/lucasmarciano/composeproject/features/sales/SalesUIState.kt
+++
@@ -1,9 +0,0 @@
-package com.lucasmarciano.composeproject.features.sales
-
-import com.lucasmarciano.composeproject.data.home.models.ResponseBuildVO
-
-sealed class SalesUIState {
-    object Loading : SalesUIState()
-    class Success(val data: ResponseBuildVO) : SalesUIState()
-    class Error(val error: Throwable) : SalesUIState()
-}
