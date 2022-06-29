e8d4d8eb1a5cd43b3d090f4ac7bc5fc3b682dbc8
lucas-marciano <lucas.almeida.marciano@gmail.com>
2022-06-29 09:31:29 -0300

    :sparkles: insert Robot fonts

--- uicomponents/src/main/java/com/lucasmarciano/ui/iti/theme/typo/Fonts.kt
+++ uicomponents/src/main/java/com/lucasmarciano/ui/iti/theme/typo/Fonts.kt
@@ -13,3 +13,18 @@ internal val nunitoFont = FontFamily(
     Font(R.font.nunito, weight = FontWeight.Thin),
     Font(R.font.nunito_italic, style = FontStyle.Italic),
 )
+
+internal val roboto = FontFamily(
+    Font(R.font.roboto_regular, weight = FontWeight.Normal),
+    Font(R.font.roboto_italic, style = FontStyle.Italic),
+    Font(R.font.roboto_black, weight = FontWeight.Black),
+    Font(R.font.roboto_black_italic, weight = FontWeight.Black, style = FontStyle.Italic),
+    Font(R.font.roboto_bold, weight = FontWeight.Bold),
+    Font(R.font.roboto_bold_italic, weight = FontWeight.Bold, style = FontStyle.Italic),
+    Font(R.font.roboto_light, weight = FontWeight.Light),
+    Font(R.font.roboto_light_italic, weight = FontWeight.Light, style = FontStyle.Italic),
+    Font(R.font.roboto_medium, weight = FontWeight.Medium),
+    Font(R.font.roboto_medium_italic, weight = FontWeight.Medium, style = FontStyle.Italic),
+    Font(R.font.roboto_thin, weight = FontWeight.Thin),
+    Font(R.font.roboto_thin_italic, weight = FontWeight.Thin, style = FontStyle.Italic),
+)
--- uicomponents/src/main/java/com/lucasmarciano/ui/iti/theme/typo/Type.kt
+++ uicomponents/src/main/java/com/lucasmarciano/ui/iti/theme/typo/Type.kt
@@ -7,46 +7,46 @@ import androidx.compose.ui.text.style.TextAlign
 
 data class Type(
     val body1: TextStyle = TextStyle(
-        fontFamily = nunitoFont,
+        fontFamily = roboto,
         fontWeight = FontWeight.SemiBold,
         fontSize = FontSize().medium,
         textAlign = TextAlign.Start
     ),
     val body2: TextStyle = TextStyle(
-        fontFamily = nunitoFont,
+        fontFamily = roboto,
         fontWeight = FontWeight.Thin,
         fontSize = FontSize().minimum,
         textAlign = TextAlign.Start
     ),
     val subtitle1: TextStyle = TextStyle(
-        fontFamily = nunitoFont,
+        fontFamily = roboto,
         fontWeight = FontWeight.Normal,
         fontSize = FontSize().large,
         letterSpacing = FontSize().letterSpacingDefault,
         textAlign = TextAlign.Start
     ),
     val subtitle2: TextStyle = TextStyle(
-        fontFamily = nunitoFont,
+        fontFamily = roboto,
         fontWeight = FontWeight.Normal,
         fontSize = FontSize().small,
         textAlign = TextAlign.Center
     ),
     val h1: TextStyle = TextStyle(
-        fontFamily = nunitoFont,
+        fontFamily = roboto,
         fontSize = FontSize().midLarge,
         letterSpacing = FontSize().letterSpacingDefault,
         textAlign = TextAlign.Start,
         fontWeight = FontWeight.Bold,
     ),
     val h2: TextStyle = TextStyle(
-        fontFamily = nunitoFont,
+        fontFamily = roboto,
         fontSize = FontSize().large,
         letterSpacing = FontSize().letterSpacingDefault,
         textAlign = TextAlign.Start,
         fontWeight = FontWeight.Bold,
     ),
     val button: TextStyle = TextStyle(
-        fontFamily = nunitoFont,
+        fontFamily = roboto,
         fontSize = FontSize().minimum,
         textAlign = TextAlign.Center,
         fontWeight = FontWeight.Bold,
