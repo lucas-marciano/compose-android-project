68ff4588ac7a832b9e654b84ddf1e66ec94a7e61
lucas-marciano <lucas.almeida.marciano@gmail.com>
2022-07-04 09:54:57 -0300

    🔥 remove file Test

---
+++ iti-module/src/main/java/com/lucasmarciano/iti_module/Teste.kt
@@ -0,0 +1,21 @@
+package com.lucasmarciano.iti_module
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ComponentTest() {
+    Text(
+        text = "test",
+        style = ItiTheme.type.body1,
+        fontSize = ItiTheme.fontSize.extraLarge,
+        color = ItiTheme.colors.variation
+    )
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    ComponentTest()
+}
